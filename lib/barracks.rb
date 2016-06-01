5695bdf95907a1cf6d9ab474a0703dda006da4fc
Jen <jen@reiher.ca>
2016-06-01 01:14:13 +0000

    tests 1-9 pass except the last one in 4

--- lib/barracks.rb
+++ lib/barracks.rb
@@ -7,18 +7,32 @@ class Barracks
 		@food = 80
 	end
 
-		def train_footman
-			if can_train_footman?
-				gold -= 135
-				food -= 2
-				Footman.new
-			else
-				nil
-			end
+	def train_footman
+		if can_train_footman?
+			@gold -= 135
+			@food -= 2
+			Footman.new
+		else
+			nil
 		end
-	  
-	  def can_train_footman?
-	    gold > 135 && food > 2
-	  end
+	end
+  
+  def can_train_footman?
+    gold > 135 && food > 2
+  end
+
+  def train_peasant
+  	if can_train_peasant?
+  		@gold -= 90
+  		@food -= 5
+  		Peasant.new
+  	else
+  		nil
+  	end
+  end
+
+  def can_train_peasant?
+  	gold > 90 && food > 5
+  end
 
 end
